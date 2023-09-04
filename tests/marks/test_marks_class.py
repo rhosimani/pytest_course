7af112059e985ec27396137637e2b7920c1c8de2
rho <rhosimani@gmail.com>
2023-09-04 09:04:19 -0300

    Uso de marcas para organizar y agrupar test

---
+++ tests/marks/test_marks_class.py
@@ -0,0 +1,14 @@
+import pytest
+
+# To run tests use:
+# poetry run pytest -v -m test_class
+
+@pytest.mark.test_class
+class TestClass:
+
+    def test_startup(self):
+        pass
+
+    def test_startup_and_more(self):
+        pass
+
